19c56cb736bcad9394e10e501e0c442d8ef8895e
Ars Vladimirov <grokhael@gmail.com>
2023-08-02 19:01:30 +0600

    summ

--- src/settings.py
+++ src/settings.py
@@ -27,8 +27,8 @@ CATEGORY: Category = Category.General(benchmark=True, featured=True)
 
 CV_TASKS: List[CVTask] = [
     CVTask.InstanceSegmentation(),
-    CVTask.ObjectDetection(),
     CVTask.SemanticSegmentation(),
+    CVTask.ObjectDetection(),
 ]
 ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.InstanceSegmentation()]
 
